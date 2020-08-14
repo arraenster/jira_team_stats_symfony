0def9f2124e142e0d15f305dc231687e145915f7
Vladyslav Semerenko <vladyslav@snaex.ch>
2020-08-14 15:43:35 +0300

    Sprint TODO update

--- src/Entity/Sprint.php
+++ src/Entity/Sprint.php
@@ -5,4 +5,13 @@ namespace App\Entity;
 class Sprint
 {
     protected $users;
+
+    protected $storyPointsAtBeginning;
+    protected $storyPointsAtEnd;
+
+    protected $scopeChange;
+
+    //TODO: Implement sprint object to keep information organized
+    // This should have the possibility to show SP per user at the beginning of the sprint and at the end
+    // This also should show all possible scope change and calculate percents of completion
 }
